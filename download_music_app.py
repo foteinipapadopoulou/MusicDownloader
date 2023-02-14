902f6e1620c81d438419336a6be2093d07abe7a0
foteinipapadopoulou <fotinipapad@hotmail.com>
2023-02-15 01:02:40 +0200

    Exception handling

--- download_music_app.py
+++ download_music_app.py
@@ -1,3 +1,4 @@
+import traceback
 from flask import Flask, request, render_template, make_response, jsonify
 import requests
 import re
@@ -7,7 +8,7 @@ import logging
 from YoutubeProvider import YoutubeProvider
 
 app = Flask(__name__)
-logging.basicConfig(filename='record.log', level=logging.DEBUG,
+logging.basicConfig(filename='../record.log', level=logging.DEBUG,
                     format=f'%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
 
 ALLOWED_HOSTS = [
@@ -15,6 +16,48 @@ ALLOWED_HOSTS = [
     "www.youtube.com",
     "youtu.be"
 ]
+
+def validate_url(url):
+    if not (url and url.strip()):
+        raise APIBadRequestError('Url value is empty')
+
+    # Check that the link is a valid URL
+    if not re.match(r'^https?://.+$', url):
+        raise APIBadRequestError('Invalid Url')
+
+    # Check that the link is to an allowed host
+    parsed_link = requests.utils.urlparse(url)
+    if parsed_link.hostname not in ALLOWED_HOSTS:
+        raise APIBadRequestError('Unallowed host in Url. Only Youtube is supported')
+
+
+class APIError(Exception):
+    """All custom API Exceptions"""
+    pass
+
+
+class APIBadRequestError(APIError):
+    """Bad Request Error Class."""
+    code = 400
+    description = "Bad request Error"
+
+
+class APIInternalServerError(APIError):
+    """Internal Server Error Class."""
+    code = 500
+    description = "Internal Server Error"
+
+
+@app.errorhandler(APIError)
+def handle_exception(err):
+    """Return a JSON with response the error message when an APIError is raised"""
+    app.logger.error(f"Unknown Exception: {str(err)}")
+    app.logger.debug(traceback.format_exc())
+    response = {"error": err.description, "message": ""}
+    if len(err.args) > 0:
+        response["message"] = err.args[0]
+        app.logger.error(f'{err.description}: {response["message"]}')
+    return jsonify(response), err.code
 
 
 @app.route('/')
@@ -25,21 +68,11 @@ def index():
 
 @app.route('/download_audio', methods=["POST"])
 def download_audio():
-    try:
-        url = request.form['url']
-    except Exception as e:
-        app.logger.error('Exception : {}'.format(str(e)))
-        return "Error: request body not correct", 400
-    if url is None:
-        return "Error: URL not provided", 400
-    # Check that the link is a valid URL
-    if not re.match(r'^https?://.+$', url):
-        return "Error: Invalid URL", 400
+    if 'url' not in request.form:
+        raise APIBadRequestError('Missing url value')
+    url = request.form['url']
 
-    # Check that the link is to an allowed host
-    parsed_link = requests.utils.urlparse(url)
-    if parsed_link.hostname not in ALLOWED_HOSTS:
-        return "Error: Unallowed host", 400
+    validate_url(url)
 
     # Fetch the audio data from Youtube
     youtube_provider = YoutubeProvider("Youtube")
@@ -47,16 +80,23 @@ def download_audio():
         content, title = youtube_provider.download_audio(url=url)
     except Exception as e:
         app.logger.error('Exception : {}'.format(str(e)))
-        return 'Error: Internal Server Error while downloading music', 500
+        raise APIInternalServerError('Internal Server Error while downloading music')
 
     with open(content, 'rb') as f:
         audio_content = f.read()
 
     # Return the MP3 data as a download
-    response = make_response(audio_content)
+    try:
+        response = make_response(audio_content)
+    except:
+        raise APIInternalServerError('Internal Server Error while returning the response')
 
     # Remove the data that are stored in the output folder
-    os.remove(content)
+    try:
+        os.remove(content)
+    except:
+        app.logger.error('Cannot remove the song:{}'.format(content))
+
     response.headers["Content-Disposition"] = 'attachment; filename="{}.mp3"'.format(title.encode('latin-1', 'ignore'))
     response.headers["Content-Type"] = "audio/mpeg"
     return response, 200
@@ -66,35 +106,25 @@ def download_audio():
 def get_audio_title():
     # Get the query parameters from the request
     query_params = request.args
-    try:
-        # Access specific query parameters by key
-        url = query_params.get("url")
-    except Exception as e:
-        app.logger.error('Exception : {}'.format(str(e)))
-        return "Error: request body not correct", 400
-    if url is None:
-        return "Error: URL not provided", 400
-    # Check that the link is a valid URL
-    if not re.match(r'^https?://.+$', url):
-        return "Error: Invalid URL", 400
+    if 'url' not in query_params:
+        raise APIBadRequestError('Missing url value')
+    url = query_params.get("url")
 
-    # Check that the link is to an allowed host
-    parsed_link = requests.utils.urlparse(url)
-    if parsed_link.hostname not in ALLOWED_HOSTS:
-        return "Error: Unallowed host", 400
-
+    validate_url(url)
     # Fetch the audio data from Youtube
     youtube_provider = YoutubeProvider("Youtube")
     try:
         yt = youtube_provider.get_youtube_url_info(url=url)
     except Exception as e:
         app.logger.error('Exception : {}'.format(str(e)))
-        return 'Error: Internal Server Error while getting the title', 500
+        raise APIInternalServerError('Internal Server Error while getting the title')
 
     title = ""
     # Return the music title
     if yt is not None and hasattr(yt, "title") and yt.title is not None:
         title = yt.title
+    else:
+        raise APIInternalServerError('Internal Server Error while returning the title')
 
     return jsonify({"title": "{}".format(title), "status": "success"})
 
--- static/js/script.js
+++ static/js/script.js
@@ -1,10 +1,13 @@
 const downloadForm = document.getElementById("download-form");
 const urlInput = document.getElementById("url-input")
 const progressElement =  document.querySelector('.progress-group');
+const formGroupElement =  document.querySelector('.form-group');
 const successMessage = document.querySelector('#successMessage');
 const failureMessage = document.querySelector('#failureMessage');
 const progressBar = document.querySelector('#progressBar');
+const songTitleToBeDownloadedWrapper = document.querySelector('.songTitleToBeDownloaded-wrapper');
 const songTitleToBeDownloaded = document.querySelector('#songTitleToBeDownloaded');
+const DEFAULT_SONG_TITLE = "audio.mp3";
 
 function getAttachmentFilenameFromDisposition (disposition){
     if (disposition && disposition.indexOf('attachment') !== -1) {
@@ -33,14 +36,41 @@ function createAndClickATagElement(blob, filename){
     window.URL.revokeObjectURL(url);
 }
 
+function showFailureMessage(){
+    successMessage.classList.add('d-none');
+    progressElement.classList.add('d-none');
+    failureMessage.classList.remove('d-none');
+}
+
+function hideMessages(){
+    successMessage.classList.add("d-none");
+    failureMessage.classList.add("d-none");
+}
+
+function showInvalidUrlInputPrompt(){
+    urlInput.classList.remove('is-valid');
+    urlInput.classList.add('is-invalid');
+    formGroupElement.classList.remove('has-success');
+    formGroupElement.classList.add('has-danger');
+}
+function showValidUrlInputPrompt(){
+    urlInput.classList.remove('is-invalid');
+    urlInput.classList.add('is-valid');
+    formGroupElement.classList.remove('has-danger');
+    formGroupElement.classList.add('has-success');
+}
 //Download Button
 downloadForm.addEventListener("submit", function(event) {
     event.preventDefault();
     //Hide any previous message
-    successMessage.classList.add("d-none");
-    failureMessage.classList.add("d-none");
-
-    let url = urlInput.value;
+    hideMessages();
+    if(isInvalidOrEmptyUrl()){
+        showInvalidUrlInputPrompt();
+        return;
+    } else {
+        showValidUrlInputPrompt();
+    }
+    let url = urlInput.value.trim();
     const xhr = new XMLHttpRequest();
 
     xhr.open("POST", "/download_audio");
@@ -53,15 +83,27 @@ downloadForm.addEventListener("submit", function(event) {
              let blob = xhr.response;
              successMessage.classList.remove('d-none');
              //Get the title of song
-             let disposition = xhr.getResponseHeader("Content-Disposition")
-             let songName = getAttachmentFilenameFromDisposition(disposition)
+             let disposition = xhr.getResponseHeader("Content-Disposition");
+             let songName = DEFAULT_SONG_TITLE;
+             if (songTitleToBeDownloaded.textContent) {
+                 songName = `${songTitleToBeDownloaded.textContent}.mp3`;
+             } else {
+                 songName = getAttachmentFilenameFromDisposition(disposition);
+             }
              createAndClickATagElement(blob, songName);
-         } else {
-             progressElement.classList.add('d-none');
-            failureMessage.classList.remove('d-none');
+         }  else if (xhr.status === 400) {
+            showInvalidUrlInputPrompt();
+         }  else {
+             showFailureMessage();
         }
     });
 
+    xhr.addEventListener('error', function (e) {
+        showFailureMessage();
+    });
+    xhr.addEventListener('abort', function (e) {
+        showFailureMessage();
+    });
 
     //Add the progress bar
     progressElement.classList.remove('d-none');
@@ -71,21 +113,34 @@ downloadForm.addEventListener("submit", function(event) {
       const percent = (e.loaded / e.total) * 100;
       progressBar.style.width = percent + '%';
     });
-
     xhr.send("url=" + encodeURIComponent(url));
 });
 
-urlInput.addEventListener('input', function(e){
+urlInput.addEventListener('keyup', function(e){
     const xhr = new XMLHttpRequest();
+    if(isInvalidOrEmptyUrl()){
+        showInvalidUrlInputPrompt();
+        return;
+    } else {
+        showValidUrlInputPrompt();
+    }
+    songTitleToBeDownloadedWrapper.classList.add("d-none");
 
-      xhr.open('GET', `/get_audio?url=${e.target.value}`, true);
+      xhr.open('GET', `/get_audio?url=${e.target.value.trim()}`, true);
 
       xhr.addEventListener('load', function(e) {
           if (xhr.status === 200){
               const response = JSON.parse(xhr.responseText);
               songTitleToBeDownloaded.textContent = response.title;
-          }
+              songTitleToBeDownloadedWrapper.classList.remove("d-none");
+          } else if (xhr.status === 400) {
+            showInvalidUrlInputPrompt();
+        }
       });
 
       xhr.send();
 })
+
+function isInvalidOrEmptyUrl(){
+     return !urlInput.value || !urlInput.checkValidity();
+}
